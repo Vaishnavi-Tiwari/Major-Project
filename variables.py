99473e5e7341b1b72e606c0243f0e945c6449f69
sanikass <77910204+sanikass@users.noreply.github.com>
2022-02-21 14:16:44 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,10 @@
+"""
+Contains variables for image-size, path of models and labels that are required by other modules
+"""
+IMAGE_SIZE = 50  # We'll be working with 50 * 50 pixel images
+MODEL_PATH = "aslmodel.h5"
+
+LABELS = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n','o', 'p', 'q','r', 's', 't','u', 'v', 'w', 'x', 'y', 'z']
+
+# Minimum confidence percentage i.e allowed for prediction
+THRESHOLD = 25
